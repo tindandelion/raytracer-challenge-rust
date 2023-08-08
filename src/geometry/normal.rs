a6d8d1280c83981bb816fdcb0992114cbf8d8538
Sergey Moshnikov <s.moshnikov@gmail.com>
2023-08-08 11:16:18 +0300

    Implement reflection calculation

--- src/geometry/normal.rs
+++ src/geometry/normal.rs
@@ -15,6 +15,10 @@ impl Normal {
     pub fn new(x: f64, y: f64, z: f64) -> Normal {
         Self::from(&Vector(x, y, z))
     }
+
+    pub fn reflect(&self, incoming: &Vector) -> Vector {
+        incoming - &self.direction * 2. * incoming.dot(&self.direction)
+    }
 }
 
 #[cfg(test)]
@@ -31,5 +35,23 @@ mod tests {
         let n = Normal::from(&v);
 
         assert_eq!(n.direction, Vector(SQRT_2 / 2., SQRT_2 / 2., 0.));
+    }
+
+    #[test]
+    fn reflect_vector_at_45_degrees() {
+        let v = Vector(1., -1., 0.);
+        let n = Normal::new(0., 1., 0.);
+
+        let r = n.reflect(&v);
+        assert_eq!(r, Vector(1., 1., 0.));
+    }
+
+    #[test]
+    fn reflect_off_slanted_surface() {
+        let v = Vector(0., -1., 0.);
+        let n = Normal::new(SQRT_2 / 2., SQRT_2 / 2., 0.);
+
+        let r = n.reflect(&v);
+        assert_eq!(r, Vector(1., 0., 0.))
     }
 }
