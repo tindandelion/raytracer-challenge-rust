c476eb55044418810786147225eececef5f19f4e
Sergey Moshnikov <s.moshnikov@gmail.com>
2023-08-25 06:50:36 +0300

    Refactoring main

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::f64::consts::PI;
 
 use drawing::Canvas;
 use drawing::Color;
+use geometry::Normal;
 use geometry::Point;
-use geometry::UnitVector;
 
 use geometry::Vector;
 use ppm::write_ppm;
@@ -25,6 +25,44 @@ struct RayHit<'a> {
     shape: &'a Sphere,
 }
 
+struct World {
+    light: PointLight,
+    shape: Sphere,
+}
+
+impl RayHit<'_> {
+    fn normal(&self) -> Normal {
+        self.shape.normal_at(&self.point)
+    }
+}
+
+impl World {
+    fn hit_with_ray(&self, ray: &Ray) -> Option<RayHit> {
+        let intersections = self.shape.intersect_with(ray);
+        if intersections.is_empty() {
+            return None;
+        } else {
+            let first_intersection = intersections[0];
+            let hit_point = ray.position(first_intersection);
+            let hit = RayHit {
+                point: hit_point,
+                shape: &self.shape,
+            };
+            return Some(hit);
+        }
+    }
+
+    fn get_color(&self, ray: &Ray) -> Option<Color> {
+        self.hit_with_ray(&ray).map(|hit| {
+            let normal = hit.normal();
+            let eye_direction = ray.direction.flip();
+            hit.shape
+                .material()
+                .lighting(&self.light, &hit.point, &eye_direction, &normal)
+        })
+    }
+}
+
 fn scan(canvas_size: usize, mut f: impl FnMut(&Ray, usize, usize) -> ()) {
     let mut camera = Camera::new(canvas_size, canvas_size, PI / 2.);
     let camera_pos = Point::new(0., 0., -1.75);
@@ -39,29 +77,6 @@ fn scan(canvas_size: usize, mut f: impl FnMut(&Ray, usize, usize) -> ()) {
     }
 }
 
-fn hit_point<'a>(r: &Ray, shape: &'a Sphere) -> Option<RayHit<'a>> {
-    let intersections = shape.intersect_with(r);
-    if intersections.is_empty() {
-        return None;
-    } else {
-        let first_intersection = intersections[0];
-        let hit_point = r.position(first_intersection);
-        let hit = RayHit {
-            point: hit_point,
-            shape,
-        };
-        return Some(hit);
-    }
-}
-
-fn get_color_at(pt: &Point, shape: &Sphere, ray_direction: &UnitVector) -> Color {
-    let light = PointLight::new(Color::WHITE, Point::new(10., 10., -10.));
-    let normal = shape.normal_at(pt);
-    shape
-        .material()
-        .lighting(&light, &pt, &(ray_direction.flip()), &normal)
-}
-
 fn sphere_material() -> Material {
     let color = Color::new(1., 0.2, 1.);
     Material::default_with_color(color)
@@ -69,13 +84,16 @@ fn sphere_material() -> Material {
 
 fn main() {
     let sphere = Sphere::unit().with_material(sphere_material());
+    let world = World {
+        light: PointLight::new(Color::WHITE, Point::new(10., 10., -10.)),
+        shape: sphere,
+    };
+
     let mut canvas = Canvas::square(512);
 
     scan(canvas.width(), |ray, px, py| {
-        if let Some(hit) = hit_point(&ray, &sphere) {
-            let point_color = get_color_at(&hit.point, hit.shape, &ray.direction);
-            canvas.write_pixel(px, py, &point_color);
-        }
+        let point_color = world.get_color(&ray).unwrap_or(Color::BLACK);
+        canvas.write_pixel(px, py, &point_color);
     });
     write_ppm("output/test-output.ppm", &canvas).unwrap();
 }
